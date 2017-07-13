fe766deb4be711deeac2f365a85416cbedc66d94
chickendude <pokemaster103@gmail.com>
2017-07-13 18:18:25 -0400

    Added bottom navigation view

--- app/src/main/java/ch/ralena/nasapp/MainActivity.kt
+++ app/src/main/java/ch/ralena/nasapp/MainActivity.kt
@@ -1,27 +1,52 @@
 package ch.ralena.nasapp
 
 import android.os.Bundle
+import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v7.app.AppCompatActivity
 import ch.ralena.nasapp.fragments.BACKSTACK_PHOTOPICK
-import ch.ralena.nasapp.fragments.MainFragment
+import ch.ralena.nasapp.fragments.EyeInTheSkyFragment
+import ch.ralena.nasapp.fragments.PostcardFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), FragmentManager.OnBackStackChangedListener {
+
+	var postcardFragment: Fragment? = null
+	var eyeInTheSkyFragment: Fragment? = null
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
 
 		supportFragmentManager.addOnBackStackChangedListener(this::onBackStackChanged)
 
+		bottomNavigationView.setOnNavigationItemSelectedListener {
+			val fragment: Fragment?
+			if (it.itemId == R.id.marsRover) {
+				if (postcardFragment == null) {
+					postcardFragment = PostcardFragment()
+				}
+				fragment = postcardFragment
+			} else {
+				if (eyeInTheSkyFragment == null) {
+					eyeInTheSkyFragment = EyeInTheSkyFragment()
+				}
+				fragment = eyeInTheSkyFragment
+			}
+			supportFragmentManager.beginTransaction()
+					.replace(R.id.fragmentContainer, fragment)
+					.commit()
+			true
+		}
+
 		// load main fragment
-		val fragment = MainFragment()
+		if (postcardFragment == null) {
+			postcardFragment = PostcardFragment()
+		}
+		val fragment = postcardFragment
 		supportFragmentManager.beginTransaction()
 				.replace(R.id.fragmentContainer, fragment)
 				.commit()
-	}
-
-	fun setUpViewPager() {
-
 	}
 
 	override fun onBackStackChanged() {
