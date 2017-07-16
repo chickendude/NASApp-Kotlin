8c657026ae85dbb12f5cb8980395f0f94d2cb3c0
chickendude <pokemaster103@gmail.com>
2017-07-16 16:15:59 -0400

    Clicking on a camera selects it.

--- app/src/main/java/ch/ralena/nasapp/adapters/CameraAdapter.kt
+++ app/src/main/java/ch/ralena/nasapp/adapters/CameraAdapter.kt
@@ -1,5 +1,6 @@
 package ch.ralena.nasapp.adapters
 
+import android.graphics.drawable.Drawable
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
@@ -7,9 +8,14 @@ import android.widget.TextView
 import ch.ralena.nasapp.R
 import ch.ralena.nasapp.fragments.cameraNames
 import ch.ralena.nasapp.inflate
+import org.jetbrains.anko.backgroundDrawable
+import org.jetbrains.anko.sdk25.coroutines.onClick
 
 class CameraAdapter(var cameras: ArrayList<String>) :
 		RecyclerView.Adapter<CameraAdapter.CameraViewHolder>() {
+
+	var currentlySelected: Int = 0
+	var defaultBackground: Drawable? = null
 
 	override fun onBindViewHolder(holder: CameraViewHolder?, position: Int) {
 		holder?.bindView(position)
@@ -17,6 +23,7 @@ class CameraAdapter(var cameras: ArrayList<String>) :
 
 	override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): CameraViewHolder {
 		val view = parent!!.inflate(R.layout.item_camera)
+		defaultBackground = view.backgroundDrawable
 		return CameraViewHolder(view)
 	}
 
@@ -29,11 +36,21 @@ class CameraAdapter(var cameras: ArrayList<String>) :
 		val fullName: TextView
 
 		init {
+			defaultBackground = itemView.backgroundDrawable
 			abbreviation = itemView.findViewById(R.id.abbreviation)
 			fullName = itemView.findViewById(R.id.fullName)
 		}
 
 		fun bindView(position: Int) {
+			if (position == currentlySelected)
+				itemView.backgroundDrawable = itemView.context.getDrawable(R.drawable.bg_edittext)
+			else
+				itemView.backgroundDrawable = defaultBackground
+			itemView.onClick {
+				currentlySelected = position
+				notifyDataSetChanged()
+			}
+
 			val cameraAbbr = cameras[position]
 			abbreviation.text = cameraAbbr
 			fullName.text = cameraNames[cameraAbbr]
